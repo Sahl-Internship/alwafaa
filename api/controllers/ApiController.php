b072b7357eddfccc1214acabcc98927a72a7f755
IbrahimBaki <3baa2y@gmail.com>
2021-02-11 12:44:29 +0200

    login

---
+++ api/controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace api\controllers;
+
+
+use yii\rest\Controller;
+
+class ApiController extends Controller
+{
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
+        ];
+        return $behaviors;
+    }
+}
