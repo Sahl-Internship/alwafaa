8b9f6e342e7f33f9cde1b22184f74d94c610fb16
IbrahimBaki <3baa2y@gmail.com>
2021-03-24 16:40:40 +0200

    edit calender

--- backend/views/course/calender.php
+++ backend/views/course/calender.php
@@ -1,28 +1,4 @@
-<?php
 
-use edofre\fullcalendar\models\Event;
-use yii\helpers\Html;
-use yii\helpers\Url;
-use yii\web\JsExpression;
-use yii\widgets\DetailView;
-
-/**
- * @var yii\web\View $this
- * // * @var common\models\Course $events
- */
-?>
-<? //= edofre\fullcalendar\Fullcalendar::widget([
-//    'clientOptions' => [
-//        'eventResize' => new JsExpression("
-//                function(event, delta, revertFunc, jsEvent, ui, view) {
-//                    console.log(event.id);
-//                    console.log(delta);
-//                }
-//            "),
-//    ],
-//]);
-//?>
-<!---->
 <?= edofre\fullcalendar\Fullcalendar::widget([
     'events' => $events,
     'header' => [
@@ -32,22 +8,3 @@ use yii\widgets\DetailView;
     ],
 ]);
 ?>
-
-<? //= edofre\fullcalendar\Fullcalendar::widget([
-//    'options'       => [
-//        'id'       => 'calendar',
-//    ],
-//    'clientOptions' => [
-//        'weekNumbers' => false,
-//        'selectable'  => true,
-//        'defaultView' => 'agendaWeek',
-//        'eventResize' => new JsExpression("
-//                function(event, delta, revertFunc, jsEvent, ui, view) {
-//                    console.log(event);
-//                }
-//            "),
-//
-//    ],
-//    'events'        => Url::to(['course/calender', 'id' => 26]),
-//]);
-//?>
